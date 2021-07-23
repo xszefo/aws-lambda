ce1352c16c84926dd4fda710746fecce7cfdcb0b
xszefo <piotr.sumelka@gmail.com>
2021-07-23 13:06:47 +0200

    poprawki

--- first-s3-encounter.py
+++ first-s3-encounter.py
@@ -4,9 +4,9 @@ import boto3
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    psum_bucket_objects = s3.list_objects(Bucket='psum-s3-bucket')
+    my_bucket = s3.list_objects(Bucket='psum-s3-bucket')
     
-    for s3_file in psum_bucket_objects['Contents']:
+    for s3_file in my_bucket['Contents']:
         print(s3_file['Key'])
         
     return {
